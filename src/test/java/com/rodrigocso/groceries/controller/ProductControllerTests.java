678ded0e6622365b6b16f5c4dc7bdadfd8b3a9b1
Rodrigo Oliveira <17278178+rodrigocso@users.noreply.github.com>
2020-12-26 16:30:32 +0000

    add more controller tests

--- src/test/java/com/rodrigocso/groceries/controller/ProductControllerTests.java
+++ src/test/java/com/rodrigocso/groceries/controller/ProductControllerTests.java
@@ -13,12 +13,16 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.json.JacksonTester;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
 import static com.rodrigocso.groceries.util.ResponseBodyMatchers.responseBody;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -55,5 +59,43 @@ public class ProductControllerTests {
         mvc.perform(get("/products"))
                 .andExpect(status().isOk())
                 .andExpect(responseBody().containsObjectAsJson(mockProducts.toArray(), ProductDto[].class));
+    }
+
+    @Test
+    public void whenPostProductWithoutName_thenReturn400AndErrorResult() throws Exception {
+        mvc.perform(post("/products")
+                .contentType("application/json")
+                .content(jsonProductDto
+                        .write(new ProductDto(1, "", new BrandDto(1, "Apple")))
+                        .getJson()))
+                .andExpect(status().isBadRequest())
+                .andExpect(responseBody().containsError("name", "IS_REQUIRED"));
+
+    }
+
+    @Test
+    public void whenGetNonExistingProductById_thenReturn404() throws Exception {
+        when(productFacade.findById(1)).thenThrow(new ResponseStatusException(HttpStatus.NOT_FOUND));
+        mvc.perform(get("/products/1")).andExpect(status().isNotFound());
+    }
+
+    @Test
+    public void whenPostProductThatAlreadyExists_thenReturn409() throws Exception {
+        when(productFacade.save(any(ProductDto.class))).thenThrow(new DataIntegrityViolationException("Oops"));
+        mvc.perform(post("/products")
+                .contentType("application/json")
+                .content(jsonProductDto
+                        .write(new ProductDto(1, "MacBook", new BrandDto(1, "Apple"))).getJson()))
+                .andExpect(status().isConflict());
+    }
+
+    @Test
+    public void whenValidUpdateExistingProduct_thenReturn200() throws Exception {
+        ProductDto product = new ProductDto(1, "iPhone", new BrandDto(1, "Apple"));
+        when(productFacade.update(any(Integer.class), any(ProductDto.class))).thenReturn(product);
+        mvc.perform(put("/products/1")
+                .contentType("application/json")
+                .content(jsonProductDto.write(product).getJson()))
+                .andExpect(status().isOk());
     }
 }
