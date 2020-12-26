0aba59296afa0123aadae72caab32f6a9ff1b022
Rodrigo Oliveira <17278178+rodrigocso@users.noreply.github.com>
2020-12-26 22:07:04 +0000

    remove unused injections

--- src/test/java/com/rodrigocso/groceries/repository/BrandRepositoryTests.java
+++ src/test/java/com/rodrigocso/groceries/repository/BrandRepositoryTests.java
@@ -5,10 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.dao.DataIntegrityViolationException;
-import org.springframework.jdbc.core.JdbcTemplate;
 
-import javax.persistence.EntityManager;
-import javax.sql.DataSource;
 import javax.validation.ConstraintViolationException;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
@@ -16,16 +13,11 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @DataJpaTest
 public class BrandRepositoryTests {
-    @Autowired private DataSource dataSource;
-    @Autowired private JdbcTemplate jdbcTemplate;
-    @Autowired private EntityManager entityManager;
-    @Autowired private BrandRepository brandRepository;
+    @Autowired
+    private BrandRepository brandRepository;
 
     @Test
     public void injectedComponentsAreNotNull() {
-        assertThat(dataSource).isNotNull();
-        assertThat(jdbcTemplate).isNotNull();
-        assertThat(entityManager).isNotNull();
         assertThat(brandRepository).isNotNull();
     }
 
